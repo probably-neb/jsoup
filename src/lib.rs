744d01469707745b770ea1bbdaf14faf12cc21bb
Ben Kunkle <ben.kunkle@gmail.com>
2025-07-05 13:40:49 -0500

    fix removing last item in collection

--- src/lib.rs
+++ src/lib.rs
@@ -1577,10 +1577,22 @@ pub fn remove_index(tree: &mut crate::JsonAst, index: usize) -> Result<(), Remov
 
     let token_removal_range = index..usize::max(tree.tok_desc[index].end, index + 1);
     let mut contents_removal_range = tree.tok_span[index].clone();
+
+    if let Some(descendants_start) = Some(tree.tok_desc[index].start).filter(|&n| n != 0) {
+        contents_removal_range.end = tree.tok_span[descendants_start].end;
+    }
+
+    let prev_sibling_index = tok_prev(tree, index);
+
     if let Some(tok_next) = Some(tree.tok_next[index]).filter(|&n| n != 0) {
         contents_removal_range.end = tree.tok_span[tok_next as usize].start;
-    } else if let Some(descendants_start) = Some(tree.tok_desc[index].start).filter(|&n| n != 0) {
-        contents_removal_range.end = tree.tok_span[descendants_start].end;
+    } else if let Some(tok_prev) = prev_sibling_index {
+        let prev_end = tree.tok_span[tok_prev].end;
+        let str = std::str::from_utf8(&tree.contents[prev_end..])
+            .ok()
+            .ok_or(RemoveError::InvalidTree)?;
+        contents_removal_range.start = prev_end + str.find(',').ok_or(RemoveError::InvalidTree)?;
+        tree.tok_next[tok_prev] = tree.tok_next[index];
     }
 
     tree.tok_desc.drain(token_removal_range.clone());
@@ -1650,6 +1662,20 @@ fn item_parent_index(tree: &JsonAst, mut item_index: usize) -> Option<usize> {
     }
 
     None
+}
+
+fn tok_prev(tree: &JsonAst, item_index: usize) -> Option<usize> {
+    if item_index == 0 {
+        return None;
+    }
+    let mut index = item_index;
+    while index > 0 {
+        index -= 1;
+        if tree.tok_next[index] as usize == item_index {
+            return Some(index);
+        }
+    }
+    return None;
 }
 
 #[cfg(test)]
@@ -2121,6 +2147,7 @@ mod test {
             check("[<1>]", "[]");
             check(r#"{<"key">: "value"}"#, "{}");
             check("[1, <0>, 2]", "[1, 2]");
+            check("[<0>, 1, 2]", "[1, 2]");
             check(
                 r#"{"foo": "bar", <"baz">: "qux", "qal": "qud"}"#,
                 r#"{"foo": "bar", "qal": "qud"}"#,
@@ -2137,6 +2164,7 @@ mod test {
                 r#"{"foo": {"bar": <{"qux": "qal"}>}}"#,
                 r#"{"foo": {"bar": null}}"#,
             );
+            check(r#"{"foo": [1,2,<3>,]}"#, r#"{"foo": [1,2,]}"#)
         }
     }
 }
